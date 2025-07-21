8a7e2b5c434ed0a9176b7026c3c1aff51bf713c1
AdemCancatal <ademc4803@gmail.com>
2025-07-22 00:33:21 +0300

    Add files via upload

---
+++ Do While Practice 4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lutfen bir sayi giriniz :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sayac = 0;
+            int i = 0;
+            int f = 0;
+
+            do
+            {
+                f = (i - 1) + (i - 2);
+                Console.WriteLine(f);
+
+                i++;
+                f = 0;
+                sayac++; 
+            } while (sayac != sayi);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
