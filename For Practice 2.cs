19b6aa955bd56a9698d19938d23deea6910fe68d
AdemCancatal <ademc4803@gmail.com>
2025-07-22 00:28:58 +0300

    Add files via upload

---
+++ For Practice 2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bir sayi giriniz");   
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int sayac = 0;
+
+
+            for (int i = 2;  i <= sayi; i++)
+            {
+                if (sayi/i == 0)
+                {
+                    sayac++;
+                }
+            }
+
+            if (sayac == 2)
+            {
+                Console.WriteLine(sayi + " asal sayidir.");
+            }
+            else if (sayac > 2) 
+            {
+                Console.WriteLine(sayi + " asal degildir.");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
