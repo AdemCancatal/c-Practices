619dbfd2bb1c49a52b240038492206e85887a08d
AdemCancatal <ademc4803@gmail.com>
2025-07-22 00:28:00 +0300

    Add files via upload

---
+++ Foreach Practice 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lutfen bir bir string deger giriniz :");
+            string deger = Console.ReadLine();
+
+            char[] charList = deger.ToCharArray();
+
+            foreach (char c in charList)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
