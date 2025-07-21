13a23e106a3101d8160d3dd6c22663e59a69ba60
AdemCancatal <ademc4803@gmail.com>
2025-07-22 00:27:08 +0300

    Add files via upload

---
+++ If-Else Practice 1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Birazdan ders notlarinizi gireceksiniz..");
+            Console.ReadLine();
+
+            Console.WriteLine("Lutfen Matematik notunuzu giriniz :");
+            int Matematik = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lutfen Fizik notunuzu giriniz :");
+            int Fizik = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lutfen Turkce notunuzu giriniz :");
+            int Turkce = Convert.ToInt32(Console.ReadLine());
+
+            int ortalama = (Matematik + Fizik + Turkce) / 3;
+
+            if (ortalama >= 85)
+            {
+                Console.WriteLine("Takdir belgesi aldiniz...");
+            }
+            else if ((ortalama >= 70) && (ortalama <= 84))
+            {
+                Console.WriteLine("Tesekkur belgesi aldiniz...");
+            }
+            else if ((ortalama >= 50) && (ortalama <= 69))
+            {
+                Console.WriteLine("Duz gectiniz...");
+            }
+            else
+            {
+                Console.WriteLine("Kaldiniz");
+            }
+
+
+                Console.WriteLine("");
+
+        }
+    }
+}
