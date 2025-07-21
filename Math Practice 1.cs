95279b89619d01155303a9af4ea508fcbc865211
AdemCancatal <ademc4803@gmail.com>
2025-07-22 00:20:44 +0300

    Add files via upload

---
+++ Math Practice 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lutfen bir sayi giriniz :");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            sayi = ((sayi * (sayi + 1)) / 2);
+
+            Console.WriteLine("Girdiginiz sayiya kadfar olan sayilarin toplami :" + sayi);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
