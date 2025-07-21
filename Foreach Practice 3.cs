f4bd785f36e135e6b5057b2612557eb2549f4942
AdemCancatal <ademc4803@gmail.com>
2025-07-22 00:32:38 +0300

    Add files via upload

---
+++ Foreach Practice 3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uygulama_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] isimler = { "Ahmet", " Mehmet", "Ayse", "Hasan", "Ahmet" };
+            Console.WriteLine("Hangi ismi aramak istiyorsunuz :");
+            string isim = Console.ReadLine();
+
+            int sayac = 0;
+
+
+            foreach (string i in isimler)
+            {
+                  if (isim == i)
+                {
+                    sayac++; 
+                }
+
+            }
+            Console.WriteLine(isim + " isimli kisinin adi bu listede "+ sayac + " kere geciyor.");
+
+        }
+    }
+}
